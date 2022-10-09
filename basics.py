79fe70f6cafc54133c5fc4a09688b04bfb546e4a
toledosq <smith_chris93@hotmail.com>
2022-10-09 09:48:58 -0700

    Cropped image practice

--- basics.py
+++ basics.py
@@ -1,6 +1,10 @@
 import cv2
 import numpy as np
 
+
+def clamp(n, smallest, largest): return max(smallest, min(n, largest))
+
+
 def show_img(img, title):
     # Display image in a window and wait for user
     cv2.imshow(title, img)
@@ -22,6 +26,49 @@ def resize_img(img, scale_factor, interpolation_method=cv2.INTER_LINEAR):
                              )
     return resized_img
     
+    
+def crop_img(img, left=0, right=0, top=0, bottom=0):
+    print(f"Original shape: {img.shape}")
+    
+    img_height = img.shape[0]
+    img_width = img.shape[1]
+
+    # (height, width, channels)
+    from_left = clamp(left, 0, img_width)
+    from_right = clamp(img_width - right, 0, img_width)
+    from_top = clamp(top, 0, img_height)
+    from_bottom = clamp(img_height - bottom, 0, img_height)
+    
+    cropped_img = img[from_top:from_bottom, from_left:from_right]
+        
+    print(f"Cropped shape: {cropped_img.shape}")
+    return cropped_img
+    
+    
+def crop_img_center(img, shape: tuple):
+    # Crops to new dimensions from center
+    print(f"Original shape: {img.shape}")
+    
+    orig_y1 = img.shape[0]
+    orig_x1 = img.shape[1]
+    
+    y_diff = int((orig_y1 - shape[0]) / 2)
+    x_diff = int((orig_x1 - shape[1]) / 2)
+    
+    print("y_diff", y_diff)
+    print("x_diff", x_diff)
+    
+    y0 = clamp(0 + y_diff, 0, orig_y1)
+    y1 = clamp(orig_y1 - y_diff, 0, orig_y1)
+    
+    x0 = clamp(0 + x_diff, 0, orig_x1)
+    x1 = clamp(orig_x1 - x_diff, 0, orig_x1)
+    
+    cropped_img = img[y0:y1, x0:x1]
+    print(f"Center Cropped Shape: {cropped_img.shape}")
+    
+    return cropped_img
+
 
 def read_video(vid):
     vid_capture = cv2.VideoCapture(vid)
@@ -107,44 +154,52 @@ if __name__ == '__main__':
     cv2.imwrite('img_copy.jpg', img)
 
     # Read, display, and save image as grayscale
-    img_grayscale = cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE)
-    show_img(img_grayscale, 'Grayscale Image')
-    cv2.imwrite('img_grayscale.jpg', img_grayscale)
+    # img_grayscale = cv2.imread('img.jpg', cv2.IMREAD_GRAYSCALE)
+    # show_img(img_grayscale, 'Grayscale Image')
+    # cv2.imwrite('img_grayscale.jpg', img_grayscale)
     
     # Resize and save an image
-    downscale_factor = 0.5
-    img_downscaled = resize_img(img, downscale_factor)
-    show_img(img_downscaled, 'Downscaled Image')
-    cv2.imwrite('img_downscaled.jpg', img_downscaled)
+    # downscale_factor = 0.5
+    # img_downscaled = resize_img(img, downscale_factor)
+    # show_img(img_downscaled, 'Downscaled Image')
+    # cv2.imwrite('img_downscaled.jpg', img_downscaled)
     
-    upscale_factor = 2
-    img_upscaled = resize_img(img, upscale_factor)
-    show_img(img_upscaled, 'Upscaled Image')
-    cv2.imwrite('img_upscaled.jpg', img_upscaled)
+    # upscale_factor = 2
+    # img_upscaled = resize_img(img, upscale_factor)
+    # show_img(img_upscaled, 'Upscaled Image')
+    # cv2.imwrite('img_upscaled.jpg', img_upscaled)
+    
+    # Crop an image
+    img_cropped = crop_img(img, left=256, right=256, top=85, bottom=85)
+    show_img(img_cropped, 'Cropped Image')
+    
+    # Crop an image by providing new shape
+    img_center_cropped = crop_img_center(img, shape=(512,512))
+    show_img(img_center_cropped, 'Center Cropped Image')
     
     
     ### Video Basics ###
     
     # Read an mp4 and display the frames
-    vid_capture, fps = read_video('vid.mp4')
-    display_video_frames(vid_capture, fps)
+    # vid_capture, fps = read_video('vid.mp4')
+    # display_video_frames(vid_capture, fps)
     
     # Release lock on video file
-    vid_capture.release()
-    del vid_capture
+    # vid_capture.release()
+    # del vid_capture
     
     # Read a folder of frames and display them
-    frames_path = 'original_frames/%04d.png'
-    vid_capture, fps = read_video(frames_path)
-    if fps == 1:
-        fps = 30
-        print("FPS changed to 30")
-    display_video_frames(vid_capture, fps)
+    # frames_path = 'original_frames/%04d.png'
+    # vid_capture, fps = read_video(frames_path)
+    # if fps == 1:
+        # fps = 30
+        # print("FPS changed to 30")
+    # display_video_frames(vid_capture, fps)
     
     # Save the frames as a video
-    output_path = 'vid_from_frames.avi'
-    write_frames_as_video(vid_capture, output_path, fps)
+    # output_path = 'vid_from_frames.avi'
+    # write_frames_as_video(vid_capture, output_path, fps)
     
     # Release lock on video file
-    vid_capture.release()
+    # vid_capture.release()
     
